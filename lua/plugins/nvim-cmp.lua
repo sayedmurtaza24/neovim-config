ae8a3346a40e1394e21414413d080a0f95382d77
Murtaza <murtaza@trail.gg>
2024-03-20 10:34:36 +0100

    fix nvim-cmp

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -8,7 +8,6 @@ return {
       return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
     end
 
-    local luasnip = require("luasnip")
     local cmp = require("cmp")
 
     opts.mapping = vim.tbl_extend("force", opts.mapping, {
@@ -18,8 +17,6 @@ return {
           cmp.select_next_item()
         -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
         -- this way you will only jump inside the snippet region
-        elseif luasnip.expand_or_locally_jumpable() then
-          luasnip.expand_or_jump()
         elseif has_words_before() then
           cmp.complete()
         else
@@ -29,8 +26,6 @@ return {
       ["<S-Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_prev_item()
-        elseif luasnip.jumpable(-1) then
-          luasnip.jump(-1)
         else
           fallback()
         end
